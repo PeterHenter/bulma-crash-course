3b4178dca2bf4e755e9c8c2e59fafb57768ef748
Amoeba notebook <peterhenter@yahoo.co.uk>
2020-11-14 17:20:40 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Code to toggle the visibility of the elements in the mobile menu.
+const burgerIcon = document.querySelector('#burger');
+const navbarMenu = document.querySelector('#nav-links');
+
+burgerIcon.addEventListener('click', ()=>{
+    // I don't really know this function, but it seems to toggle the 
+    // class that's in the parameter.
+    navbarMenu.classList.toggle('is-active');
+});
