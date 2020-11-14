472a2d855886761cb3d2db7d4deb0c0459aa941f
Amoeba notebook <peterhenter@yahoo.co.uk>
2020-11-14 17:49:34 +0000

    Solving the issue of disappearing menus

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const navbarMenu = document.querySelector('#nav-links');
 burgerIcon.addEventListener('click', ()=>{
     // I don't really know this function, but it seems to toggle the 
     // class that's in the parameter.
-    navbarMenu.classList.toggle('is-active');
+    navbarMenu.classList.toggle("is-active");
 });
