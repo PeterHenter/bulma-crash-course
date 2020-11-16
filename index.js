59e507975f3750ced0936b6864935b20f2d11d64
Amoeba notebook <peterhenter@yahoo.co.uk>
2020-11-16 19:15:07 +0000

    Course finished

--- index.js
+++ index.js
@@ -2,8 +2,49 @@
 const burgerIcon = document.querySelector('#burger');
 const navbarMenu = document.querySelector('#nav-links');
 
-burgerIcon.addEventListener('click', ()=>{
+burgerIcon.addEventListener('click', () => {
     // I don't really know this function, but it seems to toggle the 
     // class that's in the parameter.
     navbarMenu.classList.toggle("is-active");
+    burgerIcon.classList.toggle('is-a(ctive');
 });
+
+// Code to toggle the appearance of tab bars, and display the tab content of 
+// the active tab only.
+const tabBars = document.querySelectorAll('div.tabs.is-boxed > ul > li');
+const tabContents = document.querySelectorAll('#tab-content > div');
+
+tabBars.forEach((tabBar) => {
+    tabBar.addEventListener('click', () => {
+        tabBars.forEach(tabBar => tabBar.classList.toggle('is-active'));
+        tabContents.forEach(tabContent => tabContent.classList.toggle('is-hidden'));
+    })
+});
+
+// Alternatively we can use data tag attributes in the involved elements 
+// to create the same effect:
+// const tabs = document.querySelectorAll('.tabs li');
+// const tabContentBoxes = document.querySelectorAll('#tab-content > div');
+
+// tabs.forEach(tab => {
+//     tab.addEventListener('click', () => {
+//         tabs.forEach(item => item.classList.toggle('is-active'));
+//         tab.classList.add('is-active');
+
+//         const target = tab.dataset.target;
+//         tabContentBoxes.forEach(box => {
+//             if (box.getAttribute('id') === target) {
+//                 box.classList.remove('is-hidden');
+//             } else {
+//                 box.classList.add('is-hidden');
+//             }
+//         })
+//     });
+// });
+
+// Code to display the modal form, when the user clicks the 'Sign up' button. 
+const signUpBtn = document.querySelector('#sign-up-button');
+const modalForm = document.querySelector('.modal');
+
+
+signUpBtn.addEventListener('click', () => modalForm.classList.add('is-active'));
